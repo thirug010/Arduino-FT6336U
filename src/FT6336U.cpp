66487e8931295ab6ea3adb9cbf2b949d593df075
thirug010 <30732819+thirug010@users.noreply.github.com>
2022-05-21 17:44:12 -0500

    Update FT6336U.cpp

--- src/FT6336U.cpp
+++ src/FT6336U.cpp
@@ -64,8 +64,9 @@ uint16_t FT6336U::read_touch1_x(void) {
     read_buf[0] = readByte(FT6336U_ADDR_TOUCH1_X) ;
     read_buf[1] = readByte(FT6336U_ADDR_TOUCH1_X + 1);
 	//read_buf[0] <<= 8;
-	return ((read_buf[0] & 0x0f) << 8) | read_buf[1];
+	//return ((read_buf[0] & 0x0f) << 8) | read_buf[1];
 	//return read_buf[0] | read_buf[1];
+	return ((read_buf[0] & 0x0f) << 8);
 }
 uint16_t FT6336U::read_touch1_y(void) {
     uint8_t read_buf[2];
@@ -91,8 +92,8 @@ uint16_t FT6336U::read_touch2_x(void) {
     read_buf[0] = readByte(FT6336U_ADDR_TOUCH2_X) ;
     read_buf[1] = readByte(FT6336U_ADDR_TOUCH2_X + 1);
 	//read_buf[0] <<= 8;
-	return ((read_buf[0] & 0x0f) << 8) | read_buf[1];
-	//return read_buf[0] | read_buf[1];
+	//return ((read_buf[0] & 0x0f) << 8) | read_buf[1];
+	return ((read_buf[0] & 0x0f) << 8);
 }
 uint16_t FT6336U::read_touch2_y(void) {
     uint8_t read_buf[2];
